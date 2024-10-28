18ebb2e032900e48818d9735dab28fc31221ad68
heejoo <heejookim99@gmail.com>
2024-10-28 17:33:22 +0900

    feat:frontend 개발환경세팅 기초

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex w-full flex-col gap-10 px-12 py-5">
+      <p className="flex font-bold text-red-600">Hello!</p>
+    </div>
+  );
+}
+
+export default App;
