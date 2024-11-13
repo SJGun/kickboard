cc768a2f260caee8aa8fc962bc4cd2131aaa8dcb
jiyoung <jiyoung990601@gmail.com>
2024-11-13 15:56:37 +0900

    fix: CORS에러로 일단 임시로 포트 다 열어두기

--- backend/kickboard/src/main/java/kb/user/internal/config/SecurityConfig.java
+++ backend/kickboard/src/main/java/kb/user/internal/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173")); // 프론트엔드 주소
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173")); // 프론트엔드 주소
+        configuration.setAllowedOrigins(List.of("*")); // 우선 포트 다 열어두기
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
