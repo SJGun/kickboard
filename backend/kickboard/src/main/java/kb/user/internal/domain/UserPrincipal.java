baeb409ffba3c45fb5a76502dfbf5533270a50a2
채기훈 <chae0738@naver.com>
2024-11-14 10:12:03 +0900

    Feat: AuthenticationPrincipal 추가 #S11P31C108-130

---
+++ backend/kickboard/src/main/java/kb/user/internal/domain/UserPrincipal.java
@@ -0,0 +1,21 @@
+package kb.user.internal.domain;
+
+import lombok.Getter;
+
+/**
+ * 인증 전용 DTO
+ * @since JDK21
+ * @author 채기훈
+ * @param userId
+ * @param email
+ * @param role
+ * @param location
+ */
+
+@Getter
+public record UserPrincipal(Long userId, String email, UserRole role, Location location) {
+
+    public static UserPrincipal from(User user) {
+        return new UserPrincipal(user.getUserId(), user.getEmail(), user.getRole(), user.getLocation());
+    }
+}
