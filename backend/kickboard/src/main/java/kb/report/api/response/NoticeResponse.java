e6b6f2da5b0a75857365c433c45957c31d135eae
Soyoung <bayleaf07@naver.com>
2024-11-15 15:38:32 +0900

    feat: 공지사항 수정

---
+++ backend/kickboard/src/main/java/kb/report/api/response/NoticeResponse.java
@@ -0,0 +1,56 @@
+package kb.report.api.response;
+
+public class NoticeResponse {
+
+    // 공지사항 응답 객체
+    public static class Response {
+        private boolean success;
+        private Data data;
+        private String errorMessage;
+
+        // 생성자, getter, setter
+        public Response(boolean success, Data data, String errorMessage) {
+            this.success = success;
+            this.data = data;
+            this.errorMessage = errorMessage;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public Data getData() {
+            return data;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
+    // 공지사항 데이터 객체
+    public static class Data {
+        private Long id;
+        private String title;
+        private String content;
+
+        // 생성자, getter, setter
+        public Data(Long id, String title, String content) {
+            this.id = id;
+            this.title = title;
+            this.content = content;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+}
