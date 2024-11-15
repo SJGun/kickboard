d38c39579689e9bdad842fcd78c8ba1301e318f2
채기훈 <chae0738@naver.com>
2024-11-15 11:55:25 +0900

    Add: 수정본

--- backend/kickboard/src/main/java/kb/user/internal/config/SecurityConfig.java
+++ backend/kickboard/src/main/java/kb/user/internal/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:80", "http://k11c108.p.ssafy.io")); // 프론트엔드 주소
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:80", "http://k11c108.p.ssafy.io","https://k11c108.p.ssafy.io")); // 프론트엔드 주소
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
